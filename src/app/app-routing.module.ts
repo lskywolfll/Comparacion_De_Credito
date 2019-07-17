59713440d9d44835cfd4cf1c61931259e7d880dc
Jorge <jcalquin.sg@gmail.com>
2019-07-17 12:41:12 -0400

    Register (falta boton y confirmacion de datos) -  index modificacion (dependencias link bootstra,  logo)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { AdministracionComponent } from './components/administracion/administracion.component';
 import { InfoUserComponent } from './components/info-user/info-user.component';
 import { CreateBankComponent } from './components/create-bank/create-bank.component';
+import { RegistrarseComponent } from './components/registrarse/registrarse.component';
 
 const routes: Routes = [
   {path: "", component:AppComponent},
   {path: "login", component:LoginComponent},
   {path: "login/Administracion", component:AdministracionComponent},
   {path: "user/informaciones", component:InfoUserComponent},
-  {path: "createCredit", component:CreateBankComponent}
+  {path: "createCredit", component:CreateBankComponent},
+  {path: "register", component:RegistrarseComponent}
 ];  
 
 @NgModule({
