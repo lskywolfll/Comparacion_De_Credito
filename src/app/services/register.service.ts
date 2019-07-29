9f1a99d6a72f633399dee3a6a113221383783e6f
René <renealejandrosanchezmorales@gmail.com>
2019-07-28 23:52:55 -0400

    Modificaciones minimas en el uso de ngForm y servicio
    Se añade una descripcion breve sobre el uso de los servicios

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -9,18 +9,17 @@ export class RegisterService {
 
   constructor( private http:HttpClient ) { }
   // Las propiedades son iguales con el nombre que estan definidas en el backend
-  registrarEnServidor(/*nombre:string, apellido:string, rut:string, fecha_de_nacimiento:Date, genero:string, sueldo_bruto:number, correo:string, password:string*/user){
-    return this.http.post("http://localhost:9090/api/users/createUser",/*{
-      // Parametros para enviarlo a ala api(en la base de datos)
-      nombre:nombre,
-      apellido:apellido,
-      rut:rut,
-      fecha_de_nacimiento:fecha_de_nacimiento,
-      genero:genero,
-      sueldo_bruto:sueldo_bruto,
-      correo:correo,
-      password:password
-    }*/user)
+  /*nombre:string, apellido:string, rut:string, fecha_de_nacimiento:Date, genero:string, sueldo_bruto:number, correo:string, password:string*/
+  registrarEnServidor(user){
+    return this.http.post("http://localhost:9090/api/users/createUser",user)
   }
-
+  // Parametros para enviarlo a ala api(en la base de datos)
+  // nombre:nombre,
+  // apellido:apellido,
+  // rut:rut,
+  // fecha_de_nacimiento:fecha_de_nacimiento,
+  // genero:genero,
+  // sueldo_bruto:sueldo_bruto,
+  // correo:correo,
+  // password:password
 }
