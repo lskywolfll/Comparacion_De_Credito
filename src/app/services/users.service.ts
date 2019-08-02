6b19e239e04b74c51be6ad31913fbd7697ed0b05
René <renealejandrosanchezmorales@gmail.com>
2019-08-02 13:09:39 -0400

    Se añade inicio de sesion y guardado de datos del usuario en su browser
    Se reconfiguro tnato el servicio del users y la estructura del html para poder recibir todos los datos del usuario

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -6,24 +6,24 @@ import { User } from '../Modelo/User';
   providedIn: 'root'
 })
 export class UsersService {
-  private isUserLoggedIn;
-  private userLogged:User;
+  // private isUserLoggedIn;
+  // private userLogged:User;
 
   constructor(private http: HttpClient) { 
-    this.isUserLoggedIn = false;
+    // this.isUserLoggedIn = false;
   }
 
-  //Mantener sesion
-  setUserLogedIn(user:User){
-    this.isUserLoggedIn = true;
-    this.userLogged = user;
-    // Palabra clave y valor setItem("clave",valor)
-    localStorage.setItem('currentUser', JSON.stringify(user));
-  }
+  // //Mantener sesion
+  // setUserLogedIn(user:User){
+  //   this.isUserLoggedIn = true;
+  //   this.userLogged = user;
+  //   // Palabra clave y valor setItem("clave",valor)
+  //   localStorage.setItem('currentUser', JSON.stringify(user));
+  // }
 
-  getUserLogeedIn(){
-    return JSON.parse(localStorage.getItem('currentUser'));
-  }
+  // getUserLogeedIn(){
+  //   return JSON.parse(localStorage.getItem('currentUser'));
+  // }
 
   logged(correo:string,password:string){
     return this.http.get<User>(`http://localhost:9090/api/users/logged/${correo}/${password}`);
@@ -44,11 +44,8 @@ export class UsersService {
     })
   }
 
-  login(correo: string, password: string) {
-    return this.http.post(`http://localhost:9090/api/users/login`, {
-      correo: correo,
-      password: password
-    })
+  login(user) {
+    return this.http.post<User>(`http://localhost:9090/api/users/login`, user)
   }
   //`` -> sirve para poder usar las funciones ES6 para poder usar una forma de concatenacion con variables de forma mas sencilla ej:var nombre = "name" y console.log(`mi nombre es${nombre}`), ${variable}
   deleteUser(rut: string) {
