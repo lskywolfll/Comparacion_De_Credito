cc724453561dd4d538ccd318dccabe91c2aff4d5
Jorge <jcalquin.sg@gmail.com>
2019-08-02 21:11:13 -0400

    Interface createCredit adicion de img

--- src/app/components/create-bank/create-bank.component.ts
+++ src/app/components/create-bank/create-bank.component.ts
@@ -29,4 +29,29 @@ export class CreateBankComponent implements OnInit {
       console.log(e)
     }
   }
+
+  // ------- IMG PREVIEW ---------
+
+  public imagePath;
+  imgURL: any;
+  public message: string;
+ 
+  preview(files) {
+    if (files.length === 0)
+      return;
+ 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+ 
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]); 
+    reader.onload = (_event) => { 
+      this.imgURL = reader.result; 
+    }
+  }
 }
+
