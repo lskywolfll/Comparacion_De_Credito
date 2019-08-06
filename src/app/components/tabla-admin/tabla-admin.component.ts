42db571c1fda6af6fdb446a5965aa3546579be29
René <renealejandrosanchezmorales@gmail.com>
2019-08-06 00:23:29 -0400

    Arreglo en la tabla del admin
    No se podia visualizar los creditos creador u ya estaban ya pre-establecidos para visualizar.
    
    Se uso el servicio respectivo de los creditos conjunto a un arreglo de Creditos(Modelo del objeto) para tener una lista de creditos

--- src/app/components/tabla-admin/tabla-admin.component.ts
+++ src/app/components/tabla-admin/tabla-admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Credit } from 'src/app/Modelo/Credit';
+import { CreditService } from 'src/app/services/credit.service';
 
 @Component({
   selector: 'app-tabla-admin',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablaAdminComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service:CreditService) { }
 
   ngOnInit() {
+    this.service.getListAll().subscribe(data =>{
+      this.credits = data;
+      console.log(data)
+    }, err =>{
+      console.log(err);
+    })
   }
+
+  credits:Credit[];
+
 
 }
